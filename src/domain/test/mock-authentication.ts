2d8b99f50f7ad7bb15be0a3d972c2ed64d6f399b
Vini <viniciosgois17@gmail.com>
2021-12-17 09:17:01 -0300

    test: ensure RemoteAuthentication returns an Account on success

--- src/domain/test/mock-authentication.ts
+++
@@ -1,8 +0,0 @@
-import { Authentication, AuthenticationParams } from "@/domain/usecases/authentication";
-import faker from 'faker'
-
-export const mockAuthentication = (): AuthenticationParams => ({
-  email: faker.internet.email(),
-  password: faker.internet.password()
-
-})
